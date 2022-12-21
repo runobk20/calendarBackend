8d71cd23ce11d465f4d1ea1517689ab08473b91e
Bruno <lipontaleme@gmail.com>
2022-12-21 19:58:49 -0300

    Intento de corrección dos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path')
 
 // Crear el servidor de Express
 const app = express();
@@ -14,7 +15,8 @@ dbConnection();
 app.use(cors());
 
 //Directorio publico
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Lectura y parseo del body
 app.use(express.json());
@@ -27,10 +29,16 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 // Evitar que no se encuentren las rutas de react router dom
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
+ */
 
+//Internet solution
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 // Escuchar peticiones
 
